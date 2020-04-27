52f5794c3c9175c53e83731f6f2d08e36922ac75
Chiho Kim <ethan828@live.com>
2020-04-27 13:45:38 -0700

    we don't need likers and scrappers

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,6 +6,4 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     has_many :likes, dependent: :destroy
     has_many :scraps, dependent: :destroy
-    has_many :users, through: :likes
-    has_many :users, through: :scraps
 end
